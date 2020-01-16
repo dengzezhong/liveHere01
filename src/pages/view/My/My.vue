d040c8ffe442262c7f2709818cb3aaa7f8d8f12f
liguoqiangdada <54129491+liguoqiangdada@users.noreply.github.com>
2020-01-16 10:36:52 +0800

    李国强
    2020.1.16

--- src/pages/view/My/My.vue
+++ src/pages/view/My/My.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
-        我的展示区0001
+        <img src="https://t8.baidu.com/it/u=1484500186,1503043093&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1579746163&t=ca6ecd5bda87217240e1ad9279ab0378" alt="">
+        <div class="top">
+            <p>江南强少</p>
+            <p>杭州魅力新城</p>
+        </div>
     </div>
 </template>
-
+<style lang="stylus" scoped>
+    div 
+        float left
+        img
+            float left
+            width 0.6rem
+            height  0.6rem
+            border-radius 50%
+        .top
+            float left
+</style>
