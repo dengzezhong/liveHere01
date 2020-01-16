a9f56eab6cce4c4d0ae8bb06ff40fdb3c0f213a3
liguoqiangdada <54129491+liguoqiangdada@users.noreply.github.com>
2020-01-16 15:30:40 +0800

    李国强
    2020.1.16

--- src/pages/view/My/My.vue
+++ src/pages/view/My/My.vue
@@ -1,20 +1,11 @@
 <template>
-    <div>
-        <img src="https://t8.baidu.com/it/u=1484500186,1503043093&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1579746163&t=ca6ecd5bda87217240e1ad9279ab0378" alt="">
-        <div class="top">
-            <p>江南强少</p>
-            <p>杭州魅力新城</p>
-        </div>
-    </div>
+  <Usemame />
 </template>
-<style lang="stylus" scoped>
-    div 
-        float left
-        img
-            float left
-            width 0.6rem
-            height  0.6rem
-            border-radius 50%
-        .top
-            float left
-</style>
+<script>
+import Usemame from "./MyUsername";
+export default {
+  components: {
+    Usemame
+  }
+};
+</script>
---
+++ src/pages/view/My/MyUsername.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <img
+      src="https://t8.baidu.com/it/u=1484500186,1503043093&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1579746163&t=ca6ecd5bda87217240e1ad9279ab0378"
+      alt
+    />
+    <div class="top">
+       <router-link to="../Home">江南强少</router-link>
+       <br>
+       <router-link to="../Fair">杭州魅力新城111</router-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+   
+}
+</script>
+<style lang="stylus" scoped>
+div {
+  float: left; 
+
+  img {
+    float: left;
+    width: 0.6rem;
+    height: 0.6rem;
+    border-radius: 50%;
+  }
+
+  .top {
+    float: left;
+  }
+}
+</style>
