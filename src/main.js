fef10af84524e54cd89a4d1e1168835b16d844a3
xieyin-gihub <55133295+xieyin-gihub@users.noreply.github.com>
2020-01-16 09:10:30 +0800

    Xieyin
    通用样式 更新

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router/index'
 import './assets/js/rem'
 import Vant from 'vant'
 import 'vant/lib/index.css'
+import './assets/css/assets_use.styl'
 
 Vue.use(Vant)
 
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
