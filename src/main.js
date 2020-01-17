56565154001a5353c023bf2c42ced55501d5080e
dengzezhong <2207008527@qq.com>
2020-01-17 09:30:45 +0800

    dengzezhong02
    测试

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import './assets/js/rem'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import './assets/css/assets_use.styl'
+import './assets/font/iconfont.css'
 Vue.use(Vant)
 
 
 new Vue({
-  router,
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,74 +1,86 @@
 // 引入组件
 import Home from '../pages/view/Home/Home'
-import Fair from '../pages/view/Fair/Fair'
+import Fair from '../pages/view/Fair/Fair_index/Fair_index'
 import My from '../pages/view/My/My'
 import Steward from '../pages/view/Steward/Steward'
 import Community from '../pages/view/Community/Community'
 import Welcome from '../pages/view/Steward/Steward_welcome'
 import Invite from '../pages/view/Home/Services/Invite.vue'
 import toInvite from '../pages/view/Home/Services/toInvite.vue'
+import UserServe from '../pages/view/Home/UserServer/UserServe'
+import Open from '../pages/view/Home/UserServer/Open'
 
 // 路由表
 const routes = [{
-    path: '/home',
-    component: Home,
-    meta:{
-        footerShow:true
+        path: '/home',
+        component: Home,
+        meta: {
+            footerShow: true
+        }
+    },
+    {
+        path: '/fair',
+        component: Fair,
+        meta: {
+            footerShow: true
+        }
+    },
+    {
+        path: '/my',
+        component: My,
+        meta: {
+            footerShow: true
+        }
+    },
+    {
+        path: '/steward',
+        component: Steward,
+        // children: [{
+        //     name: 'welcome',
+        //     path: '/steward/welcome',
+        //     component: Welcome
+        // }]
+        meta: {
+            footerShow: true
+        }
+    },
+    {
+        name: 'welcome',
+        path: '/steward/welcome',
+        component: Welcome
+    },
+    {
+        path: '/community',
+        component: Community,
+        meta: {
+            footerShow: true
+        }
+    },
+    {
+        path: '*',
+        redirect: '/home',
+        meta: {
+            footerShow: true
+        }
+    },
+    {
+        path: '/invite',
+        component: Invite
+    },
+    {
+        path: '/toInvite',
+        component: toInvite
+    },
+    // 首页内，便民服务内路由
+    {
+        path: '/userserve',
+        component: UserServe
+    },
+    {
+        path: '/open',
+        component: Open
     }
-},
-{
-    path: '/fair',
-    component: Fair,
-    meta:{
-        footerShow:true
-    }
-},
-{
-    path: '/my',
-    component: My,
-    meta:{
-        footerShow:true
-    }
-},
-{
-    path: '/steward',
-    component: Steward,
-    // children: [{
-    //     name: 'welcome',
-    //     path: '/steward/welcome',
-    //     component: Welcome
-    // }]
-    meta:{
-        footerShow:true
-    }
-},
-{
-    name: 'welcome',
-    path: '/steward/welcome',
-    component: Welcome
-},
-{
-    path: '/community',
-    component: Community,
-    meta:{
-        footerShow:true
-    }
-},
-{
-    path: '*',
-    redirect: '/home',
-    meta:{
-        footerShow:true
-    }
-},
-{
-    path:'/invite',
-    component:Invite
-},
-{
-    path:'/toInvite',
-    component:toInvite
-}
+
 ]
 
 // 暴露路由表
