e24a927d4d59bbc5b4274b32441ab4b1e187ad76
dengzezhong <2207008527@qq.com>
2020-01-17 14:15:22 +0800

    dengzezhong
    666666

--- src/assets/js/rem.js
+++ src/assets/js/rem.js
@@ -1,4 +1,7 @@
+function rem() {
+    const docE = document.documentElement;
+    docE.style.fontSize = (docE.clientWidth / 7.5) + "px";
 }
 
-addEventListener("load", rem)
-addEventListener("resize", rem)
+addEventListener("load", rem);
+addEventListener("resize", rem);
