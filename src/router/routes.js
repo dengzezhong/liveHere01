99532766abde74f8dbe5b5823495c2937cf303fa
dengzezhong <2207008527@qq.com>
2020-01-20 19:37:36 +0800

    dengzezhong
    解决合并问题

--- src/router/routes.js
+++ src/router/routes.js
@@ -11,117 +11,134 @@ import Open from '../pages/view/Home/UserServer/Open'
 
 // 路由表
 const routes = [{
-  path: '/home',
-  component: Home,
-  meta: {
-    footerShow: true
-  }
-},
-{
-  //商城首页//
-  path: '/fair',
-  component: () => import("../pages/view/Fair/Fair_index/Fair_index"),
-  meta: {
-    footerShow: true
-  },
-  children: [
-    {//集市区块
-      path: "market",
-      component: () => import("../pages/view/Fair/Fair_index/index_market"),
-      name: "market",
-      meta: {
-        marketMain: true,//市集区
-        communityMain: false,//社区拼团区
-        convenienceMain: false,//便利区
-        footerShow: true
-      },
-      components: {
-        market: () => import("../pages/view/Fair/Fair_index/index_market"),
-      }
+        path: '/home',
+        component: Home,
+        meta: {
+            footerShow: true
+        }
     },
-    {//便利区块
-      path: "convenience",
-      component: () => import("../pages/view/Fair/Fair_index/index_convenience"),
-      name: "convenience",
-      meta: {
-        marketMain: false,//市集区
-        communityMain: true,//社区拼团区
-        convenienceMain: false,//便利区
-        footerShow: true
-      },
-      components: {
-        market: () => import("../pages/view/Fair/Fair_index/index_convenience"),
-      }
+    {
+        //商城首页//
+        path: '/fair',
+        component: () =>
+            import ("../pages/view/Fair/Fair_index/Fair_index"),
+        meta: {
+            footerShow: true
+        },
+        children: [{ //集市区块
+                path: "market",
+                component: () =>
+                    import ("../pages/view/Fair/Fair_index/index_market"),
+                name: "market",
+                meta: {
+                    marketMain: true, //市集区
+                    communityMain: false, //社区拼团区
+                    convenienceMain: false, //便利区
+                    footerShow: true
+                },
+                components: {
+                    market: () =>
+                        import ("../pages/view/Fair/Fair_index/index_market"),
+                }
+            },
+            { //便利区块
+                path: "convenience",
+                component: () =>
+                    import ("../pages/view/Fair/Fair_index/index_convenience"),
+                name: "convenience",
+                meta: {
+                    marketMain: false, //市集区
+                    communityMain: true, //社区拼团区
+                    convenienceMain: false, //便利区
+                    footerShow: true
+                },
+                components: {
+                    market: () =>
+                        import ("../pages/view/Fair/Fair_index/index_convenience"),
+                }
+            },
+            { //社区活动区块
+                path: "fair_community",
+                component: () =>
+                    import ("../pages/view/Fair/Fair_index/index_community"),
+                name: "community",
+                meta: {
+                    marketMain: false, //市集区
+                    communityMain: false, //社区拼团区
+                    convenienceMain: true, //便利区
+                    footerShow: true
+                },
+                components: {
+                    market: () =>
+                        import ("../pages/view/Fair/Fair_index/index_community"),
+                }
+            },
+        ]
     },
-    {//社区活动区块
-      path: "fair_community",
-      component: () => import("../pages/view/Fair/Fair_index/index_community"),
-      name: "community",
-      meta: {
-        marketMain: false,//市集区
-        communityMain: false,//社区拼团区
-        convenienceMain: true,//便利区
-        footerShow: true
-      },
-      components: {
-        market: () => import("../pages/view/Fair/Fair_index/index_community"),
-      }
-    },
-  ]
-},
-{
-  //商城搜索//
-  path: "/fair_search",
-  component: () => import("../pages/view/Fair/components/index_search"),
-  name: 'search',
+    {
+        //商城搜索//
+        path: "/fair_search",
+        component: () =>
+            import ("../pages/view/Fair/components/index_search"),
+        name: 'search',
 
-},
-{
-  path: '/my',
-  component: My,
-  meta: {
-    footerShow: true
-  }
-},
-{
-  path: '/steward',
-  component: Steward,
-  children: [{
-    name: 'welcome',
-    path: '/steward/welcome',
-    component: Welcome
-  }],
-  meta: {
-    footerShow: true
-  }
-},
-{
-  name: 'welcome',
-  path: '/steward/welcome',
-  component: Welcome
-},
-{
-  path: '/community',
-  component: Community,
-  meta: {
-    footerShow: true
-  }
-},
-{
-  path: '*',
-  redirect: '/home',
-  meta: {
-    footerShow: true
-  }
-},
-{
-  path: '/invite',
-  component: Invite
-},
-{
-  path: '/toInvite',
-  component: toInvite
-}
+    },
+    {
+        path: '/my',
+        component: My,
+        meta: {
+            footerShow: true
+        }
+    },
+    {
+        path: '/steward',
+        component: Steward,
+        children: [{
+            name: 'welcome',
+            path: '/steward/welcome',
+            component: Welcome
+        }],
+        meta: {
+            footerShow: true
+        }
+    },
+    {
+        name: 'welcome',
+        path: '/steward/welcome',
+        component: Welcome
+    },
+    {
+        path: '/community',
+        component: Community,
+        meta: {
+            footerShow: true
+        }
+    },
+    {
+        path: '*',
+        redirect: '/home',
+        meta: {
+            footerShow: true
+        }
+    },
+    {
+        path: '/invite',
+        component: Invite
+    },
+    {
+        path: '/toInvite',
+        component: toInvite
+    },
+    // 首页内，便民服务内路由
+    {
+        path: '/userserve',
+        component: UserServe
+    },
+    {
+        path: '/open',
+        component: Open
+    }
+
 ]
 
 // 暴露路由表
